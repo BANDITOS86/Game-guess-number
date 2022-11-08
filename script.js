52d1936eb92b296602e88d6953d87ada57642b3a
Aliaksandr <djvalentinoff@gmail.com>
2022-11-08 15:09:26 +0300

    feat: implemented the guess the number game

---
+++ script.js
@@ -0,0 +1,84 @@
+'use strict';
+
+// console.log(document.querySelector('.guess-message').textContent);
+
+// document.querySelector('.guess-message').textContent = 'Правильно!';
+
+// document.querySelector('.question').textContent = 7;
+
+// document.querySelector('.score').textContent = 11;
+
+// console.log(document.querySelector('.number-input').value);
+
+// document.querySelector('.number-input').value = 13;
+
+// console.log(document.querySelector('.number-input').value);
+
+// const eventHandler = function() {
+//   console.log(document.querySelector('.number-input').value);
+// }
+
+/*********************************************************************************** */
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+// console.log(secretNumber);
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function() {
+  const guessingNumber = Number(document.querySelector('.number-input').value);
+  // console.log(guessingNumber, typeof guessingNumber);
+
+  // No input
+  if(!guessingNumber) {
+    document.querySelector('.guess-message').textContent = 'Введите число!'; 
+
+    // Player won
+  } else if(guessingNumber === secretNumber) {
+    document.querySelector('.guess-message').textContent = 'Правильно!';
+    document.querySelector('.question').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = 'rgb(9, 250, 21)';
+    document.querySelector('.question').style.width = '50rem';
+
+    if(score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // Too high
+  } else if(guessingNumber > secretNumber) {
+
+    if(score > 1) {
+      document.querySelector('.guess-message').textContent = 'Слишком много!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.guess-message').textContent = 'Game Over!';
+    }
+
+    // Too low
+  } else if (guessingNumber < secretNumber) {
+
+    if(score > 1) {
+      document.querySelector('.guess-message').textContent = 'Слишком мало!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.guess-message').textContent = 'Game Over!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function() {
+
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+
+  document.querySelector('.question').textContent = '???';
+  document.querySelector('.question').style.width = '25rem'
+  document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)'
+  document.querySelector('.guess-message').textContent = 'Начни угадывать!';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number-input').value = '';
+})
